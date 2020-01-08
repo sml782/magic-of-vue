ecd1ad86a92dea911d3e0f959c20f7f3345d1cef
SGoldL <782015430@qq.com>
2020-01-08 21:40:14 +0800

    feat: 添加插件

--- study-vue/src/store/index.js
+++ study-vue/src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import presist from "./plugins/presist";
 import user from "./user";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // ! 严格模式，防止用户非调用式更改，更改会报错
+  strict: true,
   modules: {
     user
-  }
+  },
+  plugins: [presist]
 });
