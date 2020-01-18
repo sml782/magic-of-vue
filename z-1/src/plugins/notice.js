da2641bed69818c24ed9b3155a0273b0f9416b20
亖巠 <782015430@qq.com>
2020-01-18 20:57:44 +0800

    feat: init vue source code

--- z-1/src/plugins/notice.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import Notice from '../utils/notify';
-
-Vue.use(Notice)
--- z-2/src/srouter/vue-view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export default {
-  render(h) {
-    // this.$vnode.data.routerview = true;
-    const { routes, current } = this.$router;
-    return h(routes.getInstance(current).component);
-  }
-};
